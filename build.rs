abe177e269874d1558abae6b02bd4ba810b8a3fc
Félix Saparelli <felix@passcod.name>
2022-07-22 12:29:40 +1200

    Add windows manifest to build

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
-// Fetch build target and define this for the compiler
 fn main() {
+    embed_resource::compile("manifest.rc");
+
+    // Fetch build target and define this for the compiler
     println!(
         "cargo:rustc-env=TARGET={}",
         std::env::var("TARGET").unwrap()
