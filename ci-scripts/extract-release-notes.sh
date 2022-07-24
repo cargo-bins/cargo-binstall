c8a3f8a950c9ba64cd8ffda8568001277125a982
Félix Saparelli <felix@passcod.name>
2022-07-24 18:27:42 +1200

    Extract scripts from release workflow

---
+++ ci-scripts/extract-release-notes.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+release_pr=$(head -n1 <<< "${release_commit:-}" | jq -Rr 'split("[()]"; "")[1] // ""')
+if [[ -z "$release_pr" ]]; then
+  echo "::set-output name=notes_json::null"
+  exit
+fi
+
+gh \
+  pr --repo "$GITHUB_REPO" \
+  view "$release_pr" \
+  --json body \
+  --jq '"::set-output name=notes_json::\((.body | split("### Release notes")[1] // "") | tojson)"'
+
+
---
+++ ci-scripts/pack-release-archives.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+for o in outputs/*; do
+  pushd "$o"
+
+  cp ../../LICENSE.txt ../../README.md .
+
+  target=$(basename "$o" | cut -d. -f1)
+  if grep -qE '(apple|windows)' <<< "$target"; then
+    zip "../cargo-binstall-${target}.zip" *
+  else
+    tar cv * | gzip -9 > "../cargo-binstall-${target}.tgz"
+  fi
+
+  popd
+done
