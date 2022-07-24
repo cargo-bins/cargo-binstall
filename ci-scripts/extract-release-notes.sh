130e43a83abf8521eb3d017ba299222cbab01211
Félix Saparelli <felix@passcod.name>
2022-07-24 20:02:43 +1200

    Fix release notes extractor

--- ci-scripts/extract-release-notes.sh
+++ ci-scripts/extract-release-notes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-release_pr=$(head -n1 <<< "${release_commit:-}" | jq -Rr 'split("[()]"; "")[1] // ""')
+release_pr=$(head -n1 <<< "${COMMIT_MESSAGE:-}" | jq -Rr 'split("[()]"; "")[1] // ""')
 if [[ -z "$release_pr" ]]; then
   echo "::set-output name=notes_json::null"
   exit
