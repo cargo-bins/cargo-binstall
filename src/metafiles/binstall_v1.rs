f7c798352aa379331b0ad3ef53a7d06389b08e0b
Jiahao XU <Jiahao_XU@outlook.com>
2022-07-26 21:57:57 +1000

    Enable feature serde of semver
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ src/metafiles/binstall_v1.rs
@@ -0,0 +1,15 @@
+/*
+{
+    "name": "cargo-binstall",
+    "version_req": none,
+    "current_version": "0.10.0"
+    "source": {
+        "type": "Registry",
+        "url": "https://crates.io",
+     },
+     "target": none,
+     "bins": [
+         "cargo-binstall",
+      ],
+}
+ */
