20c7b61e7a302d51c08429e6e46a3ac31b649048
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-05 20:30:03 +1000

    FIx `ci-scripts/tests.sh`: Use `--force` to force installation.
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- ci-scripts/tests.sh
+++ ci-scripts/tests.sh
@@ -19,7 +19,7 @@ done
 cargo binstall --help >/dev/null
 
 # Install binaries using `--manifest-path`
-"./$1" binstall --log-level debug --manifest-path . --no-confirm cargo-binstall
+"./$1" binstall --force --log-level debug --manifest-path . --no-confirm cargo-binstall
 # Test that the installed binaries can be run
 cargo binstall --help >/dev/null
 
@@ -28,6 +28,7 @@ min_tls=1.3
 [[ "${2:-}" == "Windows" ]] && min_tls=1.2 # WinTLS on GHA doesn't support 1.3 yet
 
 "./$1" binstall \
+    --force \
     --log-level debug \
     --secure \
     --min-tls-version $min_tls \
