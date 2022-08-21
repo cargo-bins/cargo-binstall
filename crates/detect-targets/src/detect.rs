804ab945bfae95898e4a1e1a05734a2aac72546d
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-21 16:34:55 +1000

    Refactor: Extract `detect::windows` in crate `detect-targets`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/detect-targets/src/detect.rs
+++ crates/detect-targets/src/detect.rs
@@ -77,22 +77,4 @@ mod linux;
 mod macos;
 
 #[cfg(target_os = "windows")]
-mod windows {
-    use crate::TARGET;
-    use guess_host_triple::guess_host_triple;
-
-    pub(super) fn detect_alternative_targets(target: &str) -> Option<String> {
-        let (prefix, abi) = target.rsplit_once('-').expect("Invalid target triple");
-
-        // detect abi in ["gnu", "gnullvm", ...]
-        (abi != "msvc").then(|| format!("{prefix}-msvc"))
-    }
-
-    pub(super) fn detect_targets_windows() -> Vec<String> {
-        let mut targets = vec![guess_host_triple().unwrap_or(TARGET).to_string()];
-
-        targets.extend(detect_alternative_targets(&targets[0]));
-
-        targets
-    }
-}
+mod windows;
---
+++ crates/detect-targets/src/detect/windows.rs
@@ -0,0 +1,17 @@
+use crate::TARGET;
+use guess_host_triple::guess_host_triple;
+
+pub(super) fn detect_alternative_targets(target: &str) -> Option<String> {
+    let (prefix, abi) = target.rsplit_once('-').expect("Invalid target triple");
+
+    // detect abi in ["gnu", "gnullvm", ...]
+    (abi != "msvc").then(|| format!("{prefix}-msvc"))
+}
+
+pub(super) fn detect_targets_windows() -> Vec<String> {
+    let mut targets = vec![guess_host_triple().unwrap_or(TARGET).to_string()];
+
+    targets.extend(detect_alternative_targets(&targets[0]));
+
+    targets
+}
