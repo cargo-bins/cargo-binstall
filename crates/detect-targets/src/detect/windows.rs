4be539bd16475c06f47ccfdd5058919bcb1b38a6
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-21 19:44:47 +1000

    Fix potential panic in `windows::detect_alternative_targets`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/detect-targets/src/detect/windows.rs
+++ crates/detect-targets/src/detect/windows.rs
@@ -2,7 +2,7 @@ use crate::TARGET;
 use guess_host_triple::guess_host_triple;
 
 pub(super) fn detect_alternative_targets(target: &str) -> Option<String> {
-    let (prefix, abi) = target.rsplit_once('-').expect("Invalid target triple");
+    let (prefix, abi) = target.rsplit_once('-')?;
 
     // detect abi in ["gnu", "gnullvm", ...]
     (abi != "msvc").then(|| format!("{prefix}-msvc"))
