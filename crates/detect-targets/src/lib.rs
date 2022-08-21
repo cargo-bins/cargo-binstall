bd1d21f78aad9202e01774855f7af2d48f9f1e99
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-21 21:31:47 +1000

    Enable feature "macros" of tokio in dev mode
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/detect-targets/src/lib.rs
+++ crates/detect-targets/src/lib.rs
@@ -5,6 +5,13 @@
 //!    the runtime supports glibc.
 //!  - The binary is built for x86_64-apple-darwin, but run on
 //!    aarch64-apple-darwin.
+//!
+//! This crate provides two API:
+//!  - [`detect_targets`] provides the API to get the target
+//!    at runtime, but the code is run on the current thread.
+//!  - [`get_desired_targets`] provides the API to either
+//!    parse `$target1,$target2,...` override provided by the users,
+//!    or run [`detect_targets`] in the background using [`tokio::spawn`].
 
 mod detect;
 pub use detect::detect_targets;
