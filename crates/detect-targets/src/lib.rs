ab76b035a3fd7ecb555b24b45118e4e83ac5c231
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-21 16:45:30 +1000

    Add crate doc for `detect-targets`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/detect-targets/src/lib.rs
+++ crates/detect-targets/src/lib.rs
@@ -1,3 +1,11 @@
+//! Detect the target at the runtime.
+//!
+//! Example use cases:
+//!  - The binary is built with musl libc to run on anywhere, built
+//!    the runtime supports glibc.
+//!  - The binary is built for x86_64-apple-darwin, but run on
+//!    aarch64-apple-darwin.
+
 mod detect;
 pub use detect::detect_targets;
 
