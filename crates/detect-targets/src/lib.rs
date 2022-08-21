3cb37a5817c5f16b05b239737f55705df5caf9a0
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-21 21:19:38 +1000

    Fix typo in crate doc for `detect-targets`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/detect-targets/src/lib.rs
+++ crates/detect-targets/src/lib.rs
@@ -1,7 +1,7 @@
 //! Detect the target at the runtime.
 //!
 //! Example use cases:
-//!  - The binary is built with musl libc to run on anywhere, built
+//!  - The binary is built with musl libc to run on anywhere, but
 //!    the runtime supports glibc.
 //!  - The binary is built for x86_64-apple-darwin, but run on
 //!    aarch64-apple-darwin.
