20fae4b2c730c10b2197fe0df3af1e16d9fa9a56
Félix Saparelli <felix@passcod.name>
2022-09-04 22:26:55 +1200

    Adapt release for workspace
    Only create full releases for CLI so latest URLs still work.

--- .github/scripts/extract-tag-from-release-commit.sh
+++ .github/scripts/extract-tag-from-release-commit.sh
@@ -2,5 +2,7 @@
 set -euxo pipefail
 
 message="$(head -n1 <<< "$COMMIT_MESSAGE")"
-version="$(cut -d ' ' -f 2 <<< "${message}")"
+crate="$(cut -d ' ' -f 2 <<< "${message}")"
+version="$(cut -d ' ' -f 3 <<< "${message}")"
+echo "::set-output name=crate::${crate}"
 echo "::set-output name=version::${version}"
