b2dd13f1f66ad7d29318465d2b19bc6312fafc33
Félix Saparelli <felix@passcod.name>
2022-09-10 23:01:09 +1200

    hmm

--- .github/scripts/tests.sh
+++ .github/scripts/tests.sh
@@ -2,24 +2,19 @@
 
 set -euxo pipefail
 
-bins="b3sum cargo-binstall"
-test_bins="b3sum"
-
-test_resources=".github/scripts"
-PATH="$test_resources/fake-cargo:$PATH"
-
 unset CARGO_INSTALL_ROOT
 unset CARGO_HOME
 
 # Install binaries using cargo-binstall
 # shellcheck disable=SC2086
-"./$1" binstall --log-level debug --no-confirm $bins
+"./$1" binstall --log-level debug --no-confirm b3sum cargo-binstall
 
 # Test that the installed binaries can be run
-for bin in $test_bins; do
-    "$HOME/.cargo/bin/$bin" --version
-done
+b3sum --version
+cargo-binstall --help >/dev/null
 cargo binstall --help >/dev/null
+
+test_resources=".github/scripts"
 
 # Install binaries using `--manifest-path`
 "./$1" binstall --force --log-level debug --manifest-path "$test_resources/gitlab-test-Cargo.toml" --no-confirm cargo-binstall
@@ -61,6 +56,10 @@ cargo binstall --help >/dev/null
 "./$1" binstall --no-confirm cargo-binstall@0.12.0
 "./$1" binstall --no-confirm cargo-binstall@0.12.0 | grep -q 'cargo-binstall v0.12.0 is already installed'
 "./$1" binstall --no-confirm cargo-binstall@^0.12.0 | grep -q -v 'cargo-binstall v0.12.0 is already installed'
+
+# to force failure if falling back to source
+# FIXME: remove/replace once #136 lands
+PATH="$test_resources/fake-cargo:$PATH"
 
 "./$1" binstall \
     --force \
