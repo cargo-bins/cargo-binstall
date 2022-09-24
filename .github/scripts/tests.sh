d959ac63005d9d65dfbe25e32474a4f02b448e2d
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-25 00:39:58 +1000

    Test `cargo-watch` in CI
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- .github/scripts/tests.sh
+++ .github/scripts/tests.sh
@@ -7,12 +7,13 @@ unset CARGO_HOME
 
 # Install binaries using cargo-binstall
 # shellcheck disable=SC2086
-"./$1" binstall --log-level debug --no-confirm b3sum cargo-binstall
+"./$1" binstall --log-level debug --no-confirm b3sum cargo-binstall cargo-watch
 
 # Test that the installed binaries can be run
 b3sum --version
 cargo-binstall --help >/dev/null
 cargo binstall --help >/dev/null
+cargo watch -V
 
 test_resources=".github/scripts"
 
