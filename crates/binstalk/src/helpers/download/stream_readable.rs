09b24b14b3b8e704ea6d8b75027762a4e3421f2c
Jiahao XU <Jiahao_XU@outlook.com>
2022-10-13 00:15:10 +1100

    Impl cancellation on user signal in `StreamReadable`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/binstalk/src/helpers/download/stream_readable.rs
+++ crates/binstalk/src/helpers/download/stream_readable.rs
@@ -1,24 +1,26 @@
 use std::{
     cmp::min,
+    future::Future,
     io::{self, BufRead, Read},
+    pin::Pin,
 };
 
 use bytes::{Buf, Bytes};
 use futures_util::stream::{Stream, StreamExt};
 use tokio::runtime::Handle;
 
-use crate::errors::BinstallError;
+use crate::{errors::BinstallError, helpers::signal::wait_on_cancellation_signal};
 
 /// This wraps an AsyncIterator as a `Read`able.
 /// It must be used in non-async context only,
 /// meaning you have to use it with
 /// `tokio::task::{block_in_place, spawn_blocking}` or
 /// `std::thread::spawn`.
-#[derive(Debug)]
 pub struct StreamReadable<S> {
     stream: S,
     handle: Handle,
     bytes: Bytes,
+    cancellation_future: Pin<Box<dyn Future<Output = Result<(), io::Error>> + Send>>,
 }
 
 impl<S> StreamReadable<S> {
@@ -27,6 +29,7 @@ impl<S> StreamReadable<S> {
             stream,
             handle: Handle::current(),
             bytes: Bytes::new(),
+            cancellation_future: Box::pin(wait_on_cancellation_signal()),
         }
     }
 }
@@ -56,6 +59,20 @@ where
         Ok(n)
     }
 }
+
+async fn next_stream<S, E>(stream: &mut S) -> io::Result<Option<Bytes>>
+where
+    S: Stream<Item = Result<Bytes, E>> + Unpin,
+    BinstallError: From<E>,
+{
+    stream
+        .next()
+        .await
+        .transpose()
+        .map_err(BinstallError::from)
+        .map_err(io::Error::from)
+}
+
 impl<S, E> BufRead for StreamReadable<S>
 where
     S: Stream<Item = Result<Bytes, E>> + Unpin,
@@ -65,17 +82,17 @@ where
         let bytes = &mut self.bytes;
 
         if !bytes.has_remaining() {
-            match self.handle.block_on(async { self.stream.next().await }) {
-                Some(Ok(new_bytes)) => *bytes = new_bytes,
-                Some(Err(e)) => {
-                    let e: BinstallError = e.into();
-
-                    return Err(match e {
-                        BinstallError::Io(io_error) => io_error,
-                        _ => io::Error::new(io::ErrorKind::Other, e),
-                    });
+            let option = self.handle.block_on(async {
+                tokio::select! {
+                    res = next_stream(&mut self.stream) => res,
+                    res = self.cancellation_future.as_mut() => {
+                        Err(res.err().unwrap_or_else(|| io::Error::from(BinstallError::UserAbort)))
+                    },
                 }
-                None => (),
+            })?;
+
+            if let Some(new_bytes) = option {
+                *bytes = new_bytes;
             }
         }
         Ok(&*bytes)
