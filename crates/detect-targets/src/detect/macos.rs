d2813b423e8736846de0ed72ffae9f6333af5466
Jiahao XU <Jiahao_XU@outlook.com>
2022-11-21 19:39:48 +1100

    Fix `macos::detect_alternative_targets`: Use universal only for arm or x86
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/detect-targets/src/detect/macos.rs
+++ crates/detect-targets/src/detect/macos.rs
@@ -1,16 +1,18 @@
 use crate::TARGET;
 use guess_host_triple::guess_host_triple;
-use std::iter;
 
 const AARCH64: &str = "aarch64-apple-darwin";
 const X86: &str = "x86_64-apple-darwin";
 const UNIVERSAL: &str = "universal-apple-darwin";
 
 pub(super) fn detect_alternative_targets(target: &str) -> impl Iterator<Item = String> {
-    (target == AARCH64)
+    let is_aarch64 = target == AARCH64;
+    let is_x86 = target == X86;
+
+    is_aarch64
         .then(|| X86.to_string())
         .into_iter()
-        .chain(iter::once(UNIVERSAL.to_string()))
+        .chain((is_aarch64 || is_x86).then(|| UNIVERSAL.to_string()))
 }
 
 pub(super) fn detect_targets_macos() -> Vec<String> {
