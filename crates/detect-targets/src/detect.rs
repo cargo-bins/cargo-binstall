ed8280088b17ff2e4c1542497891eacdf0edd658
Jiahao XU <Jiahao_XU@outlook.com>
2022-11-21 19:35:43 +1100

    Refactor: Extract new fn `macos::detect_alternative_targets`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/detect-targets/src/detect.rs
+++ crates/detect-targets/src/detect.rs
@@ -42,11 +42,7 @@ pub async fn detect_targets() -> Vec<String> {
                     v.push(v[0].replace("gnu", "musl"));
                 }
             } else if #[cfg(target_os = "macos")] {
-                if &*v[0] == macos::AARCH64 {
-                    v.push(macos::X86.to_string());
-                }
-
-                v.push(macos::UNIVERSAL.to_string());
+                v.extend(macos::detect_alternative_targets(&v[0]));
             } else if #[cfg(target_os = "windows")] {
                 v.extend(windows::detect_alternative_targets(&v[0]));
             }
--- crates/detect-targets/src/detect/macos.rs
+++ crates/detect-targets/src/detect/macos.rs
@@ -1,18 +1,22 @@
 use crate::TARGET;
 use guess_host_triple::guess_host_triple;
+use std::iter;
 
-pub(super) const AARCH64: &str = "aarch64-apple-darwin";
-pub(super) const X86: &str = "x86_64-apple-darwin";
-pub(super) const UNIVERSAL: &str = "universal-apple-darwin";
+const AARCH64: &str = "aarch64-apple-darwin";
+const X86: &str = "x86_64-apple-darwin";
+const UNIVERSAL: &str = "universal-apple-darwin";
+
+pub(super) fn detect_alternative_targets(target: &str) -> impl Iterator<Item = String> {
+    (target == AARCH64)
+        .then(|| X86.to_string())
+        .into_iter()
+        .chain(iter::once(UNIVERSAL.to_string()))
+}
 
 pub(super) fn detect_targets_macos() -> Vec<String> {
     let mut targets = vec![guess_host_triple().unwrap_or(TARGET).to_string()];
 
-    if targets[0] == AARCH64 {
-        targets.push(X86.to_string());
-    }
-
-    targets.push(UNIVERSAL.to_string());
+    targets.extend(detect_alternative_targets(&targets[0]));
 
     targets
 }
