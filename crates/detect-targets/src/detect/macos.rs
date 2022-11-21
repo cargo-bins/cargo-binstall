95334924cc0cb241bb53c1e41cfdba41934be755
Jiahao XU <Jiahao_XU@outlook.com>
2022-11-21 20:08:10 +1100

    Refactor `macos::detect_alternative_targets`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/detect-targets/src/detect/macos.rs
+++ crates/detect-targets/src/detect/macos.rs
@@ -6,15 +6,10 @@ const X86: &str = "x86_64-apple-darwin";
 const UNIVERSAL: &str = "universal-apple-darwin";
 
 pub(super) fn detect_alternative_targets(target: &str) -> impl Iterator<Item = String> {
-    let is_aarch64 = target == AARCH64;
-    let is_x86 = target == X86;
-
-    if is_aarch64 {
-        [Some(X86), Some(UNIVERSAL)]
-    } else if is_x86 {
-        [Some(UNIVERSAL), None]
-    } else {
-        [None, None]
+    match target {
+        AARCH64 => [Some(X86), Some(UNIVERSAL)],
+        X86 => [Some(UNIVERSAL), None],
+        _ => [None, None],
     }
     .into_iter()
     .flatten()
