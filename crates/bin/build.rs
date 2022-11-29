d64710d3a6a1c5fe0e3df0d91c96f59809a21fbd
Jiahao XU <Jiahao_XU@outlook.com>
2022-11-29 16:31:59 +1100

    Fix crates/bin/build.rs: Rerun build.rs if manifest.rc and windows.manifest changed (#575)
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/bin/build.rs
+++ crates/bin/build.rs
@@ -1,5 +1,7 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=manifest.rc");
+    println!("cargo:rerun-if-changed=windows.manifest");
 
     embed_resource::compile("manifest.rc");
 }
