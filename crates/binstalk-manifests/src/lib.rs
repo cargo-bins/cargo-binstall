03cdc5342a43dbe72278031d5a7ab35e93ad9196
Jiahao XU <Jiahao_XU@outlook.com>
2022-12-02 01:45:12 +1100

    Re-export `CompactString`, `FileLock`, `Version` & `Url` in binstalk-manifests
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/binstalk-manifests/src/lib.rs
+++ crates/binstalk-manifests/src/lib.rs
@@ -14,3 +14,7 @@ pub mod binstall_crates_v1;
 pub mod cargo_crates_v1;
 
 pub use binstalk_types::{cargo_toml_binstall, crate_info};
+pub use compact_str::CompactString;
+pub use fs_lock::FileLock;
+pub use semver::Version;
+pub use url::Url;
