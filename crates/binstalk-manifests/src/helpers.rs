ae90a39de9c05b2f1d8bb9e3146437228da2f73d
Jiahao XU <Jiahao_XU@outlook.com>
2022-12-30 15:16:10 +1100

    Optimize `create_if_not_exist`: Take `&path` to avoid dup monomorphization
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/binstalk-manifests/src/helpers.rs
+++ crates/binstalk-manifests/src/helpers.rs
@@ -1,9 +1,7 @@
 use std::{fs, io, path::Path};
 
 /// Returned file is readable and writable.
-pub(crate) fn create_if_not_exist(path: impl AsRef<Path>) -> io::Result<fs::File> {
-    let path = path.as_ref();
-
+pub(crate) fn create_if_not_exist(path: &Path) -> io::Result<fs::File> {
     let mut options = fs::File::options();
     options.read(true).write(true);
 
