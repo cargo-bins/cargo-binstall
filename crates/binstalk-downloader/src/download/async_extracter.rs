c1332c0d2dfd5d2145ed2398ece14d78e800da28
Félix Saparelli <felix@passcod.name>
2023-01-17 13:08:25 +1300

    Revert "Bump async_zip from 0.0.9 to 0.0.10" (#714)

--- crates/binstalk-downloader/src/download/async_extracter.rs
+++ crates/binstalk-downloader/src/download/async_extracter.rs
@@ -48,10 +48,8 @@ where
     let mut zip = ZipFileReader::new(reader);
     let mut buf = BytesMut::with_capacity(4 * 4096);
 
-    while let Some(mut zip_reader) = zip.next_entry().await.map_err(ZipError::from_inner)? {
-        extract_zip_entry(&mut zip_reader, path, &mut buf).await?;
-
-        zip = zip_reader.done().await.map_err(ZipError::from_inner)?;
+    while let Some(entry) = zip.entry_reader().await.map_err(ZipError::from_inner)? {
+        extract_zip_entry(entry, path, &mut buf).await?;
     }
 
     Ok(())
