2b04a66a75441da1fe71ca8408c9b2c152cb3440
Félix Saparelli <felix@passcod.name>
2023-02-11 02:46:27 +1300

    Avoid failing the build when cli is off

--- crates/leon/src/main.rs
+++ crates/leon/src/main.rs
@@ -1,45 +1,44 @@
-#![cfg(feature = "cli")]
-
-use std::{collections::HashMap, error::Error, io::stdout};
-
-use clap::Parser;
-use leon::Template;
-
-/// Render a Leon template with the given values.
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Leon template
-    template: String,
-
-    /// Default to use for missing keys
-    #[arg(long)]
-    default: Option<String>,
-
-    /// Use values from the environment
-    #[arg(long)]
-    env: bool,
-
-    /// Key-value pairs to use
-    #[arg(short, long, value_parser = parse_key_val::<String, String>)]
-    values: Vec<(String, String)>,
-}
-
-/// Parse a single key-value pair
-fn parse_key_val<T, U>(s: &str) -> Result<(T, U), Box<dyn Error + Send + Sync + 'static>>
-where
-    T: std::str::FromStr,
-    T::Err: Error + Send + Sync + 'static,
-    U: std::str::FromStr,
-    U::Err: Error + Send + Sync + 'static,
-{
-    let pos = s
-        .find('=')
-        .ok_or_else(|| format!("invalid KEY=value: no `=` found in `{s}`"))?;
-    Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
-}
-
+#[cfg(feature = "cli")]
 fn main() -> miette::Result<()> {
+    use std::{collections::HashMap, error::Error, io::stdout};
+
+    use clap::Parser;
+    use leon::Template;
+
+    /// Render a Leon template with the given values.
+    #[derive(Parser, Debug)]
+    #[command(author, version, about, long_about = None)]
+    struct Args {
+        /// Leon template
+        template: String,
+
+        /// Default to use for missing keys
+        #[arg(long)]
+        default: Option<String>,
+
+        /// Use values from the environment
+        #[arg(long)]
+        env: bool,
+
+        /// Key-value pairs to use
+        #[arg(short, long, value_parser = parse_key_val::<String, String>)]
+        values: Vec<(String, String)>,
+    }
+
+    /// Parse a single key-value pair
+    fn parse_key_val<T, U>(s: &str) -> Result<(T, U), Box<dyn Error + Send + Sync + 'static>>
+    where
+        T: std::str::FromStr,
+        T::Err: Error + Send + Sync + 'static,
+        U: std::str::FromStr,
+        U::Err: Error + Send + Sync + 'static,
+    {
+        let pos = s
+            .find('=')
+            .ok_or_else(|| format!("invalid KEY=value: no `=` found in `{s}`"))?;
+        Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
+    }
+
     let args = Args::parse();
     let mut values = HashMap::from_iter(args.values);
     if args.env {
@@ -57,3 +56,6 @@ fn main() -> miette::Result<()> {
     template.render_into(&mut stdout(), &values)?;
     Ok(())
 }
+
+#[cfg(not(feature = "cli"))]
+fn main() {}
