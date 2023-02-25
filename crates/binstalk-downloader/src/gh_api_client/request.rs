6951fbcf5654f66cbf213f1cf3c954451d4d476b
Jiahao XU <Jiahao_XU@outlook.com>
2023-02-26 00:54:20 +1100

    Simplify `fetch_release_artifacts`: Download entire json then decode it
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/binstalk-downloader/src/gh_api_client/request.rs
+++ crates/binstalk-downloader/src/gh_api_client/request.rs
@@ -2,19 +2,18 @@ use std::{collections::HashSet, io, time::Duration};
 
 use compact_str::CompactString;
 use serde::Deserialize;
-use serde_json::Deserializer as JsonDeserializer;
+use serde_json::from_slice as json_from_slice;
 use thiserror::Error as ThisError;
 use url::Url;
 
 pub use serde_json::Error as JsonError;
 
 use super::{remote, GhRelease};
-use crate::utils::{extract_with_blocking_task, StreamReadable};
 
 #[derive(ThisError, Debug)]
 pub enum GhApiError {
     #[error("IO Error: {0}")]
-    Io(#[source] io::Error),
+    Io(#[from] io::Error),
 
     #[error("Failed to parse json: {0}")]
     Json(#[from] JsonError),
@@ -24,28 +23,6 @@ pub enum GhApiError {
 
     #[error("Failed to parse url: {0}")]
     InvalidUrl(#[from] url::ParseError),
-}
-
-impl From<io::Error> for GhApiError {
-    fn from(err: io::Error) -> Self {
-        match err.get_ref() {
-            Some(inner_err) if inner_err.is::<Self>() => {
-                let inner = err.into_inner().expect(
-                    "err.get_ref() returns Some, so err.into_inner() should also return Some",
-                );
-
-                inner.downcast().map(|b| *b).unwrap()
-            }
-            Some(inner_err) if inner_err.is::<JsonError>() => {
-                let inner = err.into_inner().expect(
-                    "err.get_ref() returns Some, so err.into_inner() should also return Some",
-                );
-
-                inner.downcast().map(|b| GhApiError::Json(*b)).unwrap()
-            }
-            _ => GhApiError::Io(err),
-        }
-    }
 }
 
 // Only include fields we do care about
@@ -111,22 +88,9 @@ pub(super) async fn fetch_release_artifacts(
         return Ok(FetchReleaseRet::ReleaseNotFound);
     }
 
-    let stream = response.error_for_status()?.bytes_stream();
+    let bytes = response.error_for_status()?.bytes().await?;
 
-    // Restful API will return a lot of data and we don't want to allocate
-    // a buffer for all of them.
-    //
-    // So we instead spawn a blocking task to download and decode json
-    // from stream lazily instead of downloading everything into memory
-    // then decode it.
-    let res: Result<Response, GhApiError> = extract_with_blocking_task(stream, |rx| {
-        let reader = StreamReadable::new(rx);
-        Response::deserialize(&mut JsonDeserializer::from_reader(reader))
-            .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
-    })
-    .await;
-
-    let response = res?;
+    let response: Response = json_from_slice(&bytes)?;
 
     Ok(FetchReleaseRet::Artifacts(
         response
