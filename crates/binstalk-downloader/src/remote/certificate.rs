10e98468446086f9c010790beebceff14a3c2ff0
Jiahao XU <Jiahao_XU@outlook.com>
2023-03-02 16:39:18 +1100

    Ret `remote::Error` in `Certificate::from_*`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/binstalk-downloader/src/remote/certificate.rs
+++ crates/binstalk-downloader/src/remote/certificate.rs
@@ -1,18 +1,22 @@
 use reqwest::tls;
 
-use super::ReqwestError;
+use super::Error;
 
 #[derive(Clone, Debug)]
 pub struct Certificate(pub(super) tls::Certificate);
 
 impl Certificate {
     /// Create a Certificate from a binary DER encoded certificate
-    pub fn from_der(der: impl AsRef<[u8]>) -> Result<Self, ReqwestError> {
-        tls::Certificate::from_der(der.as_ref()).map(Self)
+    pub fn from_der(der: impl AsRef<[u8]>) -> Result<Self, Error> {
+        tls::Certificate::from_der(der.as_ref())
+            .map(Self)
+            .map_err(Error::from)
     }
 
     /// Create a Certificate from a PEM encoded certificate
-    pub fn from_pem(pem: impl AsRef<[u8]>) -> Result<Self, ReqwestError> {
-        tls::Certificate::from_pem(pem.as_ref()).map(Self)
+    pub fn from_pem(pem: impl AsRef<[u8]>) -> Result<Self, Error> {
+        tls::Certificate::from_pem(pem.as_ref())
+            .map(Self)
+            .map_err(Error::from)
     }
 }
