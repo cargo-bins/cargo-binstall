a7fcfc399232f822359ae964479d71237401eed7
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-05-29 12:22:51 +0100

    Add one-liner for installing `cargo-binstall` on Windows

---
+++ install-from-binstall-release.ps1
@@ -0,0 +1,20 @@
+$tmpdir = $Env:TEMP
+$base_url = "https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-"
+$type = (Get-ComputerInfo).CsSystemType.ToLower()
+if ($type.StartsWith("x64")) {
+	$arch = "x86_64"
+} elseif ($type.StartsWith("arm64")) {
+	$arch = "aarch64"
+} else {
+	Write-Host "Unsupported Architecture: $type" -ForegroundColor Red
+	[Environment]::Exit(1)
+}
+$url = "$base_url$arch-pc-windows-msvc.zip"
+Invoke-WebRequest $url -OutFile $tmpdir\cargo-binstall.zip
+Expand-Archive -Force $tmpdir\cargo-binstall.zip $tmpdir\cargo-binstall
+Invoke-Expression "$tmpdir\cargo-binstall\cargo-binstall.exe -y --force cargo-binstall"
+Remove-Item -Force $tmpdir\cargo-binstall.zip
+Remove-Item -Recurse -Force $tmpdir\cargo-binstall
+if ($Env:Path -split ";" -notcontains "$HOME\.cargo\bin") {
+	Write-Host "Your path is missing $HOME\.cargo\bin, you might want to add it." -ForegroundColor Red
+}
