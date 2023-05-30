e6a1484b1a94c44791a10f0af4e46629fcd82314
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-05-30 13:46:46 +0100

    Update win install script to exit on error and trace

--- install-from-binstall-release.ps1
+++ install-from-binstall-release.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+Set-PSDebug -Trace 1
 $tmpdir = $Env:TEMP
 $base_url = "https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-"
 $type = (Get-ComputerInfo).CsSystemType.ToLower()
@@ -12,9 +14,12 @@ if ($type.StartsWith("x64")) {
 $url = "$base_url$arch-pc-windows-msvc.zip"
 Invoke-WebRequest $url -OutFile $tmpdir\cargo-binstall.zip
 Expand-Archive -Force $tmpdir\cargo-binstall.zip $tmpdir\cargo-binstall
+Write-Host ""
 Invoke-Expression "$tmpdir\cargo-binstall\cargo-binstall.exe -y --force cargo-binstall"
 Remove-Item -Force $tmpdir\cargo-binstall.zip
 Remove-Item -Recurse -Force $tmpdir\cargo-binstall
 if ($Env:Path -split ";" -notcontains "$HOME\.cargo\bin") {
+	Write-Host ""
 	Write-Host "Your path is missing $HOME\.cargo\bin, you might want to add it." -ForegroundColor Red
+	Write-Host ""
 }
