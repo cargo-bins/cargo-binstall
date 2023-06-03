b619cb94205bedfaa0080e50590f9f8688fadd81
Jiahao XU <Jiahao_XU@outlook.com>
2023-06-03 16:53:50 +1000

    e2e-tests: Check version of installed crates
    and also test `fetch_crate_cratesio_version_matched` in
    `e2e-tests/live.sh`.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- e2e-tests/live.sh
+++ e2e-tests/live.sh
@@ -4,7 +4,11 @@ set -euxo pipefail
 
 unset CARGO_INSTALL_ROOT
 
-crates="b3sum@1.3.3 cargo-release@0.24.9 cargo-binstall@0.20.1 cargo-watch@8.4.0 miniserve@0.23.0 sccache@0.3.3"
+#  - `b3sum@<=1.3.3` would test `fetch_crate_cratesio_version_matched` ability
+#    to find versions matching <= 1.3.3
+#  - `cargo-quickinstall` would test `fetch_crate_cratesio_version_matched` ability
+#    to find latest stable version.
+crates="b3sum@<=1.3.3 cargo-release@0.24.9 cargo-binstall@0.20.1 cargo-watch@8.4.0 miniserve@0.23.0 sccache@0.3.3 cargo-quickinstall"
 
 CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
 export CARGO_HOME
@@ -22,9 +26,32 @@ cargo binstall --no-confirm $crates
 rm -r "$othertmpdir"
 
 # Test that the installed binaries can be run
-b3sum --version
-cargo-release release --version
+b3sum_version="$(b3sum --version)"
+echo "$b3sum_version"
+
+[ "$b3sum_version" = "b3sum 1.3.3" ]
+
+cargo_release_version="$(cargo-release release --version)"
+echo "$cargo_release_version"
+
+[ "$cargo_release_version" = "cargo-release 0.24.9" ]
+
 cargo-binstall --help >/dev/null
 cargo binstall --help >/dev/null
-cargo watch -V
-miniserve -V
+
+cargo_binstall_version="$(cargo-binstall -V)"
+echo "cargo-binstall version $cargo_binstall_version"
+
+[ "$cargo_binstall_version" = "0.20.1" ]
+
+cargo_watch_version="$(cargo watch -V)"
+echo "$cargo_watch_version"
+
+[ "$cargo_watch_version" = "cargo-watch 8.4.0" ]
+
+miniserve_version="$(miniserve -V)"
+echo "$miniserve_version"
+
+[ "$miniserve_version" = "miniserve 0.23.0" ]
+
+cargo-quickinstall -V
