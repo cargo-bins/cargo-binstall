f0a62754ade302ca2dc400bd9de43c51df815c59
Zanie Adkins <contact@zanie.dev>
2023-06-04 14:32:41 -0500

    Use `uname -s` for install script compatibility with outdated macOS GNU utils (#1123)

--- install-from-binstall-release.sh
+++ install-from-binstall-release.sh
@@ -6,12 +6,12 @@ cd "$(mktemp -d)"
 
 base_url="https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-"
 
-os="$(uname -o)"
+os="$(uname -s)"
 if [ "$os" == "Darwin" ]; then
     url="${base_url}universal-apple-darwin.zip"
     curl -LO --proto '=https' --tlsv1.2 -sSf "$url"
     unzip cargo-binstall-universal-apple-darwin.zip
-elif [ "$os" == "GNU/Linux" ]; then
+elif [ "$os" == "Linux" ]; then
     machine="$(uname -m)"
     target="${machine}-unknown-linux-musl"
     if [ "$machine" == "armv7" ]; then
