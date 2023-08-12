c57356e87039c23faa929bd69b1dafed5dfea833
Jiahao XU <Jiahao_XU@outlook.com>
2023-08-12 17:20:26 +1000

    Refactor: Move mod `git` into `binstalk-downloader` (#1285)
    To speedup codegen time for `binstalk`, also fixed the docs.rs build for
    `binstalk-downloader`.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/binstalk/src/helpers.rs
+++ crates/binstalk/src/helpers.rs
@@ -1,7 +1,5 @@
 pub(crate) mod cargo_toml_workspace;
 pub(crate) mod futures_resolver;
-#[cfg(feature = "git")]
-pub(crate) mod git;
 pub mod jobserver_client;
 pub mod remote;
 pub mod signal;
@@ -10,6 +8,9 @@ pub mod tasks;
 
 pub use binstalk_downloader::gh_api_client;
 pub(crate) use binstalk_downloader::{bytes, download};
+
+#[cfg(feature = "git")]
+pub(crate) use binstalk_downloader::git;
 
 pub(crate) fn is_universal_macos(target: &str) -> bool {
     ["universal-apple-darwin", "universal2-apple-darwin"].contains(&target)
