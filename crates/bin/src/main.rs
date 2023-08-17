5350eaaa03e05288ee8f5e010c502ec8434d60ab
Jiahao XU <Jiahao_XU@outlook.com>
2023-08-17 21:43:01 +1000

    Refactor `cargo-binstall`
     - Move implementation of `main` into the library part of
       `cargo-binstall` to speedup codegen.
     - Move  `manifests.rs` into `binstalk-manifests`
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -1,68 +1,11 @@
-use std::time::Instant;
+use std::process::Termination;
 
-use binstalk::{helpers::jobserver_client::LazyJobserverClient, TARGET};
-use log::LevelFilter;
-use tracing::debug;
-
-use cargo_binstall::{
-    args,
-    bin_util::{run_tokio_main, MainExit},
-    entry,
-    logging::logging,
-};
+use cargo_binstall::do_main;
 
 #[cfg(feature = "mimalloc")]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-fn main() -> MainExit {
-    // This must be the very first thing to happen
-    let jobserver_client = LazyJobserverClient::new();
-
-    let args = args::parse();
-
-    if args.version {
-        let cargo_binstall_version = env!("CARGO_PKG_VERSION");
-        if args.verbose {
-            let build_date = env!("VERGEN_BUILD_DATE");
-
-            let features = env!("VERGEN_CARGO_FEATURES");
-
-            let git_sha = option_env!("VERGEN_GIT_SHA").unwrap_or("UNKNOWN");
-            let git_commit_date = option_env!("VERGEN_GIT_COMMIT_DATE").unwrap_or("UNKNOWN");
-
-            let rustc_semver = env!("VERGEN_RUSTC_SEMVER");
-            let rustc_commit_hash = env!("VERGEN_RUSTC_COMMIT_HASH");
-            let rustc_llvm_version = env!("VERGEN_RUSTC_LLVM_VERSION");
-
-            println!(
-                r#"cargo-binstall: {cargo_binstall_version}
-build-date: {build_date}
-build-target: {TARGET}
-build-features: {features}
-build-commit-hash: {git_sha}
-build-commit-date: {git_commit_date}
-rustc-version: {rustc_semver}
-rustc-commit-hash: {rustc_commit_hash}
-rustc-llvm-version: {rustc_llvm_version}"#
-            );
-        } else {
-            println!("{cargo_binstall_version}");
-        }
-        MainExit::Success(None)
-    } else {
-        logging(
-            args.log_level.unwrap_or(LevelFilter::Info),
-            args.json_output,
-        );
-
-        let start = Instant::now();
-
-        let result = run_tokio_main(|| entry::install_crates(args, jobserver_client));
-
-        let done = start.elapsed();
-        debug!("run time: {done:?}");
-
-        MainExit::new(result, done)
-    }
+fn main() -> impl Termination {
+    do_main()
 }
---
+++ crates/binstalk-manifests/src/crates_manifests.rs
@@ -0,0 +1,93 @@
+use std::{
+    collections::BTreeMap,
+    fs,
+    io::{self, Seek},
+    path::Path,
+};
+
+use fs_lock::FileLock;
+use miette::Diagnostic;
+use thiserror::Error as ThisError;
+
+use crate::{
+    binstall_crates_v1::{Error as BinstallCratesV1Error, Records as BinstallCratesV1Records},
+    cargo_crates_v1::{CratesToml, CratesTomlParseError},
+    crate_info::CrateInfo,
+    CompactString, Version,
+};
+
+#[derive(Debug, Diagnostic, ThisError)]
+#[non_exhaustive]
+pub enum ManifestsError {
+    #[error("failed to parse binstall crates-v1 manifest: {0}")]
+    #[diagnostic(transparent)]
+    BinstallCratesV1(#[from] BinstallCratesV1Error),
+
+    #[error("failed to parse cargo v1 manifest: {0}")]
+    #[diagnostic(transparent)]
+    CargoManifestV1(#[from] CratesTomlParseError),
+
+    #[error("I/O error: {0}")]
+    Io(#[from] io::Error),
+}
+
+pub struct Manifests {
+    binstall: BinstallCratesV1Records,
+    cargo_crates_v1: FileLock,
+}
+
+impl Manifests {
+    pub fn open_exclusive(cargo_roots: &Path) -> Result<Self, ManifestsError> {
+        // Read cargo_binstall_metadata
+        let metadata_path = cargo_roots.join("binstall/crates-v1.json");
+        fs::create_dir_all(metadata_path.parent().unwrap())?;
+
+        let binstall = BinstallCratesV1Records::load_from_path(&metadata_path)?;
+
+        // Read cargo_install_v1_metadata
+        let manifest_path = cargo_roots.join(".crates.toml");
+
+        let cargo_crates_v1 = fs::File::options()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(manifest_path)
+            .and_then(FileLock::new_exclusive)?;
+
+        Ok(Self {
+            binstall,
+            cargo_crates_v1,
+        })
+    }
+
+    fn rewind_cargo_crates_v1(&mut self) -> Result<(), ManifestsError> {
+        self.cargo_crates_v1.rewind().map_err(ManifestsError::from)
+    }
+
+    /// `cargo-uninstall` can be called to uninstall crates,
+    /// but it only updates .crates.toml.
+    ///
+    /// So here we will honour .crates.toml only.
+    pub fn load_installed_crates(
+        &mut self,
+    ) -> Result<BTreeMap<CompactString, Version>, ManifestsError> {
+        self.rewind_cargo_crates_v1()?;
+
+        CratesToml::load_from_reader(&mut self.cargo_crates_v1)
+            .and_then(CratesToml::collect_into_crates_versions)
+            .map_err(ManifestsError::from)
+    }
+
+    pub fn update(mut self, metadata_vec: Vec<CrateInfo>) -> Result<(), ManifestsError> {
+        self.rewind_cargo_crates_v1()?;
+
+        CratesToml::append_to_file(&mut self.cargo_crates_v1, &metadata_vec)?;
+
+        for metadata in metadata_vec {
+            self.binstall.replace(metadata);
+        }
+        self.binstall.overwrite()?;
+
+        Ok(())
+    }
+}
