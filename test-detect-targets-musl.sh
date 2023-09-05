0ad08561acf34573fef0f0e6bc6c3e26f8d70681
Jiahao XU <Jiahao_XU@outlook.com>
2023-09-05 23:08:31 +1000

    Add `ls` to detect-targets-alpine-test for debugging
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test-detect-targets-musl.sh
+++ test-detect-targets-musl.sh
@@ -11,6 +11,8 @@ TARGET=${1?}
 apk update
 apk add gcompat
 
+ls -lsha /lib
+
 GNU_TARGET=$(echo "$TARGET" | sed 's/musl/gnu/')
 
 [ "$(detect-targets)" = "$(printf '%s\n%s' "$GNU_TARGET" "$TARGET")" ]
