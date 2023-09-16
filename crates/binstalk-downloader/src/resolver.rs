6560225c0362092f62dae33fe9c25a2ad5553f3f
Sculas <contact@sculas.xyz>
2023-09-17 01:51:07 +0200

    fix: use custom DNS resolver (#1364)
    * add DefaultResolver
    
    * style: fmt
    
    * chore: remove unused feature
    
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    
    * chore: remove unused `ipconfig/computer` feature
    
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    
    * chore: update `trust-dns-resolver`
    
    * refactor: apply requested changes
    
    * chore: revert `trust-dns-resolver` upgrade
    
    I mistakenly thought `reqwest` already updated to the newest version, but it hasn't! (no release was made yet)
    
    * refactor: cleanup
    
    ---------
    
    Co-authored-by: GNQG <my.t.gnq+dev@gmail.com>
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ crates/binstalk-downloader/src/resolver.rs
@@ -0,0 +1,64 @@
+use std::{net::SocketAddr, sync::Arc};
+
+use hyper::client::connect::dns::Name;
+use once_cell::sync::OnceCell;
+use reqwest::dns::{Addrs, Resolve};
+use trust_dns_resolver::config::{NameServerConfig, Protocol, ResolverConfig, ResolverOpts};
+use trust_dns_resolver::TokioAsyncResolver;
+
+#[derive(Debug, Default, Clone)]
+pub struct TrustDnsResolver(Arc<OnceCell<TokioAsyncResolver>>);
+
+impl Resolve for TrustDnsResolver {
+    fn resolve(&self, name: Name) -> reqwest::dns::Resolving {
+        let resolver = self.clone();
+        Box::pin(async move {
+            let resolver = resolver.0.get_or_try_init(new_resolver)?;
+
+            let lookup = resolver.lookup_ip(name.as_str()).await?;
+            let addrs: Addrs = Box::new(lookup.into_iter().map(|ip| SocketAddr::new(ip, 0)));
+            Ok(addrs)
+        })
+    }
+}
+
+fn new_resolver() -> Result<TokioAsyncResolver, Box<dyn std::error::Error + Send + Sync>> {
+    #[cfg(unix)]
+    {
+        let (config, opts) = trust_dns_resolver::system_conf::read_system_conf()?;
+        Ok(TokioAsyncResolver::tokio(config, opts)?)
+    }
+    #[cfg(windows)]
+    {
+        let mut config = ResolverConfig::new();
+        let opts = ResolverOpts::default();
+
+        let current_interface = default_net::get_default_interface()?;
+        ipconfig::get_adapters()?
+            .iter()
+            .filter_map(|adapter| {
+                if adapter.adapter_name() == current_interface.name {
+                    Some(adapter.dns_servers())
+                } else {
+                    None
+                }
+            })
+            .flatten()
+            .for_each(|addr| {
+                let socket_addr = SocketAddr::new(*addr, 53);
+                for protocol in [Protocol::Udp, Protocol::Tcp] {
+                    config.add_name_server(NameServerConfig {
+                        socket_addr,
+                        protocol,
+                        tls_dns_name: None,
+                        trust_nx_responses: false,
+                        #[cfg(feature = "rustls")]
+                        tls_config: None,
+                        bind_addr: None,
+                    })
+                }
+            });
+
+        Ok(TokioAsyncResolver::tokio(config, opts)?)
+    }
+}
