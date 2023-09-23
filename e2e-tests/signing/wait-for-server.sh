640b05d3ec064c3f3446d0e1953bc46c8d182091
Jiahao XU <Jiahao_XU@outlook.com>
2023-09-23 12:31:39 +1000

    Fixed infinite loop in `signing/wait-for-server` on Windows
    Pass `--ssl-revoke-best-effort` to prevent schannel from checking ssl
    revocation status.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- e2e-tests/signing/wait-for-server.sh
+++ e2e-tests/signing/wait-for-server.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 CERT="${BINSTALL_HTTPS_ROOT_CERTS?}"
 
-while ! curl --cacert "$CERT" -L https://localhost:4443/signing-test.tar | file -; do
+while ! curl --cacert "$CERT" --ssl-revoke-best-effort -L https://localhost:4443/signing-test.tar | file -; do
     sleep 10
 done
