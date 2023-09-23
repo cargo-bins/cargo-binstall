c52fb74c3a74875f56b08fbb6a5e1a2fee5294b0
Jiahao XU <Jiahao_XU@outlook.com>
2023-09-23 10:44:21 +1000

    Fix `e2e-tests/signing.sh` MacOS CI failure
    Move the tls cert creation into `signing.sh` and sleep for 10s to wait
    for https server to start.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- e2e-tests/server.py
+++ e2e-tests/server.py
@@ -2,32 +2,6 @@ import http.server
 import os
 import ssl
 
-def remove_file(path):
-    try:
-        os.remove(path)
-    except:
-        pass
-
-remove_file('ca.pem')
-remove_file('ca.srl')
-remove_file('ca.key')
-remove_file('server.csr')
-remove_file('server.ext')
-remove_file('server.pem')
-remove_file('server.key')
-os.system('openssl req -newkey rsa:4096 -x509 -sha256 -days 1 -nodes -out ca.pem -keyout ca.key -subj "/C=UT/CN=ca.localhost"')
-os.system('openssl req -new -newkey rsa:4096 -sha256 -nodes -out server.csr -keyout server.key -subj "/C=UT/CN=localhost"')
-with open('server.ext', 'w') as file:
-    file.write('''
-authorityKeyIdentifier=keyid,issuer
-basicConstraints=CA:FALSE
-keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
-subjectAltName = @alt_names
-[alt_names]
-DNS.1 = localhost
-''')
-os.system('openssl x509 -req -in server.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out server.pem -days 1 -sha256 -extfile server.ext')
-
 server_address = ('', 4443)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 ctx = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_SERVER)
--- e2e-tests/signing.sh
+++ e2e-tests/signing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eEuxo pipefail
+set -euxo pipefail
 
 unset CARGO_INSTALL_ROOT
 
@@ -8,10 +8,18 @@ CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
 export CARGO_HOME
 export PATH="$CARGO_HOME/bin:$PATH"
 
+echo Generate tls cert
+
+rm -f ca.pem ca.srl ca.key server.csr server.pem server.key
+
+openssl req -newkey rsa:4096 -x509 -sha256 -days 1 -nodes -out ca.pem -keyout ca.key -subj "/C=UT/CN=ca.localhost"
+openssl req -new -newkey rsa:4096 -sha256 -nodes -out server.csr -keyout server.key -subj "/C=UT/CN=localhost"
+openssl x509 -req -in server.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out server.pem -days 1 -sha256 -extfile server.ext
+
 python server.py 2>/dev/null &
 server_pid=$!
 trap 'kill $server_pid' ERR INT TERM
-sleep 5 # for server to come up
+sleep 10 # for server to come up
 
 export BINSTALL_HTTPS_ROOT_CERTS=$PWD/ca.pem
 
