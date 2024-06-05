d1aa7273c010d9b8ed61d37bfdac9c9c9590c04e
Jiahao XU <Jiahao_XU@outlook.com>
2024-06-06 00:39:17 +1000

    Temporarily enable debug output for e2e-test-private-github-repo
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- e2e-tests/private-github-repo.sh
+++ e2e-tests/private-github-repo.sh
@@ -10,7 +10,7 @@ export PATH="$CARGO_HOME/bin:$PATH"
 
 # Install binaries using `--manifest-path`
 # Also test default github template
-"./$1" binstall --force --manifest-path "manifests/private-github-repo-test-Cargo.toml" --no-confirm cargo-binstall --strategies crate-meta-data
+"./$1" binstall --force --manifest-path "manifests/private-github-repo-test-Cargo.toml" --no-confirm cargo-binstall --strategies crate-meta-data --log-level debug
 
 # Test that the installed binaries can be run
 cargo binstall --help >/dev/null
