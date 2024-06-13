2b57b6a83de470121fe793fe845f832804b8dcae
Remo Senekowitsch <remo@buenzli.dev>
2024-06-13 04:47:25 +0200

    chore(test): add jj-cli to live e2e test

--- e2e-tests/live.sh
+++ e2e-tests/live.sh
@@ -8,7 +8,7 @@ unset CARGO_INSTALL_ROOT
 #    to find versions matching <= 1.3.3
 #  - `cargo-quickinstall` would test `fetch_crate_cratesio_version_matched` ability
 #    to find latest stable version.
-crates="b3sum@<=1.3.3 cargo-release@0.24.9 cargo-binstall@0.20.1 cargo-watch@8.4.0 miniserve@0.23.0 sccache@0.3.3 cargo-quickinstall"
+crates="b3sum@<=1.3.3 cargo-release@0.24.9 cargo-binstall@0.20.1 cargo-watch@8.4.0 miniserve@0.23.0 sccache@0.3.3 cargo-quickinstall jj-cli@0.18.0"
 
 CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
 export CARGO_HOME
@@ -54,3 +54,8 @@ echo "$miniserve_version"
 [ "$miniserve_version" = "miniserve 0.23.0" ]
 
 cargo-quickinstall -V
+
+jj_version="$(jj --version)"
+echo "$jj_version"
+
+[ "$jj_version" = "jj 0.18.0-9fb5307b7886e390c02817af7c31b403f0279144" ]
