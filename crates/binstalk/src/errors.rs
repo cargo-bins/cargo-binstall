6c0df537a285c52b4d7048712c2f03f125669604
Jiahao XU <Jiahao_XU@outlook.com>
2024-06-14 07:50:44 +0000

    Update From<io::Error> for BinstallError

--- crates/binstalk/src/errors.rs
+++ crates/binstalk/src/errors.rs
@@ -530,20 +530,8 @@ impl Termination for BinstallError {
 
 impl From<io::Error> for BinstallError {
     fn from(err: io::Error) -> Self {
-        if err.get_ref().is_some() {
-            let kind = err.kind();
-
-            let inner = err
-                .into_inner()
-                .expect("err.get_ref() returns Some, so err.into_inner() should also return Some");
-
-            inner
-                .downcast()
-                .map(|b| *b)
-                .unwrap_or_else(|err| BinstallError::Io(io::Error::new(kind, err)))
-        } else {
-            BinstallError::Io(err)
-        }
+        err.downcast::<BinstallError>()
+            .unwrap_or_else(BinstallError::Io)  
     }
 }
 
