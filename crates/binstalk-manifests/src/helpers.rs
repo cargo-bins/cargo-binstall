90e824d57c5b5905d09ad43a9b0a7ae8c03e0523
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-02-22 22:44:17 +1100

    Update create_if_not_exist to return FileLock on successs
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- crates/binstalk-manifests/src/helpers.rs
+++ crates/binstalk-manifests/src/helpers.rs
@@ -1,7 +1,9 @@
 use std::{fs, io, path::Path};
 
-/// Returned file is readable and writable.
-pub(crate) fn create_if_not_exist(path: &Path) -> io::Result<fs::File> {
+use fs_lock::FileLock;
+
+/// Return exclusively locked file that is readable and writable.
+pub(crate) fn create_if_not_exist(path: &Path) -> io::Result<FileLock> {
     let mut options = fs::File::options();
     options.read(true).write(true);
 
@@ -10,4 +12,6 @@ pub(crate) fn create_if_not_exist(path: &Path) -> io::Result<fs::File> {
         .create_new(true)
         .open(path)
         .or_else(|_| options.open(path))
+        .and_then(FileLock::new_exclusive)
+        .map(|file_lock| file_lock.set_file_path(path))
 }
