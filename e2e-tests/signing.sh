578b5210e23c1a03981059afc507010823847ac9
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-08-30 12:59:53 +1000

    ci: disable quickinstall test in signing.sh
    since it's currently down due to maintenance of a suspicious malware
    
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/signing.sh
+++ e2e-tests/signing.sh
@@ -30,6 +30,6 @@ signing/wait-for-server.sh
 "./$1" binstall --force --manifest-path manifests/signing-Cargo.toml --no-confirm --skip-signatures signing-test
 
 # from quick-install
-"./$1" binstall --force --strategies quick-install --no-confirm --only-signed --target x86_64-unknown-linux-musl zellij@0.38.2
+#"./$1" binstall --force --strategies quick-install --no-confirm --only-signed --target x86_64-unknown-linux-musl zellij@0.38.2
 
 kill $server_pid || true
