a101049ecc0dee2edd76b94cde69fb99c0fcd244
Mikhail Katychev <mkatych@gmail.com>
2025-10-22 13:45:03 -0500

    cleanup times

--- install-from-binstall-release.ps1
+++ install-from-binstall-release.ps1
@@ -1,3 +1,4 @@
+$PSStyle.Formatting.Verbose = $PSStyle.Foreground.Cyan
 $ProgressPreference = 'SilentlyContinue'
 $ErrorActionPreference = "Stop"
 $tmpdir = $Env:TEMP
@@ -21,46 +22,26 @@ if ($proc_arch -eq "AMD64") {
 } elseif ($proc_arch -eq "ARM64") {
 	$arch = "aarch64"
 } else {
-	Write-Host "Unsupported Architecture: $type" -ForegroundColor Red
+	Write-Verbose "Unsupported Architecture: $type" -Verbose
 	[Environment]::Exit(1)
 }
 $url = "$base_url$arch-pc-windows-msvc.zip"
 # create temp with zip extension (or Expand will complain)
-Write-Host "Invoke-WebRequest"
-$sw = [Diagnostics.Stopwatch]::StartNew()
 $zip = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } –PassThru
 Invoke-WebRequest -OutFile $zip $url
 $zip | Expand-Archive -DestinationPath $tmpdir -Force
-$sw.Stop()
-$sw.Elapsed
 
-Write-Host ""
-
-Write-Host "Start-Process"
-$sw = [Diagnostics.Stopwatch]::StartNew()
 $ps = Start-Process -PassThru -Wait "$tmpdir\cargo-binstall.exe" "--self-install"
 if ($ps.ExitCode -ne 0) {
     Invoke-Expression "$tmpdir\cargo-binstall.exe -y --force cargo-binstall"
 }
-$sw.Stop()
-$sw.Elapsed
 
-Write-Host "Rm Files"
-$sw = [Diagnostics.Stopwatch]::StartNew()
 $zip | Remove-Item
-$sw.Stop()
-$sw.Elapsed
-Write-Host "Path"
-$sw = [Diagnostics.Stopwatch]::StartNew()
 $cargo_home = if ($Env:CARGO_HOME -ne $null) { $Env:CARGO_HOME } else { "$HOME\.cargo" }
 if ($Env:Path -split ";" -notcontains "$cargo_home\bin") {
     if (($Env:CI -ne $null) -and ($Env:GITHUB_PATH -ne $null)) {
         Add-Content -Path "$Env:GITHUB_PATH" -Value "$cargo_home\bin"
     } else {
-	    Write-Host ""
-    	Write-Host "Your path is missing $cargo_home\bin, you might want to add it." -ForegroundColor Red
-	    Write-Host ""
+    	Write-Verbose "Your path is missing $cargo_home\bin, you might want to add it." -Verbose
      }
 }
-$sw.Stop()
-$sw.Elapsed
