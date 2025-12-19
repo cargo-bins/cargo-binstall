c85f0eeb20318afaf27164c73f5411082b5b8675
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-12-20 00:05:59 +1100

    Replace doc_auto_cfg with doc_cfg (#2402)
    * Replace doc_auto_cfg with doc_cfg
    
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
    
    * Update
    
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
    
    * Change feature flag from 'doc_auto_cfg' to 'doccfg'
    
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
    
    * Update
    
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
    
    * Change doc_auto_cfg feature to doc_cfg
    
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- crates/binstalk-fetchers/src/lib.rs
+++ crates/binstalk-fetchers/src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 use std::{path::Path, sync::Arc, time::Duration};
 
--- crates/binstalk-registry/src/lib.rs
+++ crates/binstalk-registry/src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doccfg))]
 
 use std::{fmt, io, str::FromStr, sync::Arc};
 
