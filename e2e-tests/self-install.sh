54ae50f824d4c2e79a716d88f108d515951c98d0
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-01-17 17:11:43 +1100

    Refactor self-install script to use TMPDIR
    Update self-install script to use a temporary directory for the installation process.
    
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/self-install.sh
+++ e2e-tests/self-install.sh
@@ -8,7 +8,9 @@ CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
 export CARGO_HOME
 export PATH="$CARGO_HOME/bin:$PATH"
 
-"./$1" --self-install
+TMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t tmp)
+cp "./$1" "$TMPDIR/cargo-binstall"
+"$TMPDIR/cargo-binstall" --self-install
 
 cargo binstall -vV
 cargo install --list
