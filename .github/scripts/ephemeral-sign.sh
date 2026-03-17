ee5fe6034b39f94f529bd3b3e14f3b026e9330c7
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-17 20:44:59 +1100

    Rm decryption from ephemeral-sign.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- .github/scripts/ephemeral-sign.sh
+++ .github/scripts/ephemeral-sign.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
-[[ -z "$AGE_KEY_SECRET" ]] && { echo "!!! Empty age key secret !!!"; exit 1; }
-cat >> age.key <<< "$AGE_KEY_SECRET"
-
-set -x
-
-rage --decrypt --identity age.key --output minisign.key minisign.key.age
+set -euxo pipefail
 
 ts=$(node -e 'console.log((new Date).toISOString())')
 git=$(git rev-parse HEAD)
@@ -16,5 +9,3 @@ comment="gh=$GITHUB_REPOSITORY git=$git ts=$ts run=$GITHUB_RUN_ID"
 for file in "$@"; do
     rsign sign -W -s minisign.key -x "$file.sig" -t "$comment" "$file"
 done
-
-rm age.key minisign.key
