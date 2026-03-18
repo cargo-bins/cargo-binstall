45350aaf7cd0a676d47ec9631a2fe79c22650ff4
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-18 23:52:31 +1100

    Refactor script to remove redundant './' prefix
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/strategies.sh
+++ e2e-tests/strategies.sh
@@ -11,7 +11,7 @@ export PATH="$CARGO_HOME/bin:$PATH"
 ## Test --disable-strategies
 set +e
 
-"./$1" binstall --no-confirm --disable-strategies quick-install,compile cargo-update@11.1.2
+"$1" binstall --no-confirm --disable-strategies quick-install,compile cargo-update@11.1.2
 exit_code="$?"
 
 set -e
@@ -24,7 +24,7 @@ fi
 ## Test --strategies
 set +e
 
-"./$1" binstall --no-confirm --strategies crate-meta-data cargo-update@11.1.2
+"$1" binstall --no-confirm --strategies crate-meta-data cargo-update@11.1.2
 exit_code="$?"
 
 set -e
@@ -35,12 +35,12 @@ if [ "$exit_code" != 94 ]; then
 fi
 
 ## Test compile-only strategy
-"./$1" binstall --no-confirm --strategies compile cargo-quickinstall@0.2.8
+"$1" binstall --no-confirm --strategies compile cargo-quickinstall@0.2.8
 
 ## Test Cargo.toml disable-strategies
 set +e
 
-"./$1" binstall --no-confirm --manifest-path "manifests/strategies-test-Cargo.toml" cargo-update@11.1.2
+"$1" binstall --no-confirm --manifest-path "manifests/strategies-test-Cargo.toml" cargo-update@11.1.2
 exit_code="$?"
 
 set -e
@@ -52,7 +52,7 @@ fi
 
 set +e
 
-"./$1" binstall --disable-strategies compile --no-confirm --manifest-path "manifests/strategies-test-Cargo2.toml" cargo-update@11.1.2
+"$1" binstall --disable-strategies compile --no-confirm --manifest-path "manifests/strategies-test-Cargo2.toml" cargo-update@11.1.2
 exit_code="$?"
 
 set -e
@@ -63,4 +63,4 @@ if [ "$exit_code" != 94 ]; then
 fi
 
 ## Test --strategies overriding `disabled-strategies=["compile"]` in Cargo.toml
- "./$1" binstall --no-confirm --manifest-path "manifests/strategies-test-override-Cargo.toml" --strategies compile cargo-quickinstall@0.2.10
+"$1" binstall --no-confirm --manifest-path "manifests/strategies-test-override-Cargo.toml" --strategies compile cargo-quickinstall@0.2.10
