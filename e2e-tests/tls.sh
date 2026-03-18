e05d7e6f4cd3b250abfb5c4862cdea728965f7c6
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-18 23:53:47 +1100

    Fix command substitution for binstall in tls.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/tls.sh
+++ e2e-tests/tls.sh
@@ -8,7 +8,7 @@ CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
 export CARGO_HOME
 export PATH="$CARGO_HOME/bin:$PATH"
 
-"./$1" binstall \
+"$1" binstall \
     --force \
     --min-tls-version "${2:-1.3}" \
     --no-confirm \
