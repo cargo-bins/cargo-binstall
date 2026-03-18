84c041cf00292b24c296a316e408ff3993801706
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-19 02:27:29 +1100

    Update self-upgrade-no-symlink.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/self-upgrade-no-symlink.sh
+++ e2e-tests/self-upgrade-no-symlink.sh
@@ -18,16 +18,16 @@ cp "$1" "$CARGO_HOME/bin/cargo-binstall"
 cargo binstall --no-confirm --no-symlinks --force cargo-binstall@0.20.1
 
 # self update replacing no-symlinks with symlinks
-cp "$1" "$CARGO_HOME/bin"
+cp "$1" "$CARGO_HOME/bin/cargo-binstall"
 
 cargo binstall --no-confirm --force cargo-binstall@0.20.1
 
 # self update replacing symlinks with symlinks
-ln -snf "$(pwd)/cargo-binstall" "$CARGO_HOME/bin/cargo-binstall"
+ln -snf "$1" "$CARGO_HOME/bin/cargo-binstall"
 
 cargo binstall --no-confirm --force cargo-binstall@0.20.1
 
 # self update replacing symlinks with no-symlinks
-ln -snf "$(pwd)/cargo-binstall" "$CARGO_HOME/bin/cargo-binstall"
+ln -snf "$1" "$CARGO_HOME/bin/cargo-binstall"
 
 cargo binstall --no-confirm --force --no-symlinks cargo-binstall@0.20.1
