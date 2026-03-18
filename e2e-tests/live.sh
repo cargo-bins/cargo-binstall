ead15baae50b82af14f3f877eba55a166c2eef0d
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-19 02:14:50 +1100

    Update live.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/live.sh
+++ e2e-tests/live.sh
@@ -19,7 +19,7 @@ export PATH="$CARGO_HOME/bin:$othertmpdir/bin:$PATH"
 
 mkdir -p "$othertmpdir/bin"
 # Copy it to bin to test use of env var `CARGO`
-cp "$1" "$othertmpdir/bin/"
+cp "$1" "$othertmpdir/bin/cargo-binstall"
 
 # Install binaries using cargo-binstall
 # shellcheck disable=SC2086
