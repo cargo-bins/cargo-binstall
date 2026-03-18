bcc4d0799777aa261539d9051336e7fd88b722b9
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-18 23:54:42 +1100

    Update version-syntax.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/version-syntax.sh
+++ e2e-tests/version-syntax.sh
@@ -9,11 +9,11 @@ export CARGO_HOME
 export PATH="$CARGO_HOME/bin:$PATH"
 
 # Test --version
-"./$1" binstall --force --no-confirm --version 0.11.1 cargo-binstall
+"$1" binstall --force --no-confirm --version 0.11.1 cargo-binstall
 # Test that the installed binaries can be run
 cargo binstall --help >/dev/null
 
 # Test "$crate_name@$version"
-"./$1" binstall --force --no-confirm cargo-binstall@0.11.1
+"$1" binstall --force --no-confirm cargo-binstall@0.11.1
 # Test that the installed binaries can be run
 cargo binstall --help >/dev/null
