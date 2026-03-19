db6ba2285e256a76caf6a38fd1b925a0f256a176
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-20 01:39:11 +1100

    Update tls.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/tls.sh
+++ e2e-tests/tls.sh
@@ -2,12 +2,6 @@
 
 set -euxo pipefail
 
-unset CARGO_INSTALL_ROOT
-
-CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
-export CARGO_HOME
-export PATH="$CARGO_HOME/bin:$PATH"
-
 "$1" binstall \
     --force \
     --min-tls-version "${2:-1.3}" \
