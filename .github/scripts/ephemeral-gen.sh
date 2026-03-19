1f14acd92f72adee26b31fa26b6e046973c99cb1
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-20 02:06:00 +1100

    Simplify release-pipeline: Sign pre-builts in one workflow (#2503)
    Instead of encrypting and uploading the private key, which also has
    safety issue
    
    ---------
    
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- .github/scripts/ephemeral-gen.sh
+++ .github/scripts/ephemeral-gen.sh
@@ -7,6 +7,3 @@ rsign generate -f -W -p minisign.pub -s minisign.key
 set +x
 echo "::add-mask::$(tail -n1 minisign.key)"
 set -x
-
-rage --encrypt --recipient "$AGE_KEY_PUBLIC" --output minisign.key.age minisign.key
-rm minisign.key
