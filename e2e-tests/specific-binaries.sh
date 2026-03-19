1a820707c18f42e255578fb9668dbe2a7bbc19f6
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-20 01:36:21 +1100

    Update specific-binaries.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/specific-binaries.sh
+++ e2e-tests/specific-binaries.sh
@@ -2,12 +2,6 @@
 
 set -euxo pipefail
 
-unset CARGO_INSTALL_ROOT
-
-CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
-export CARGO_HOME
-export PATH="$CARGO_HOME/bin:$PATH"
-
 # Install a specific binary, ensuring we don't fallback to source.
 "$1" binstall --no-confirm \
     taplo-cli --bin taplo \
