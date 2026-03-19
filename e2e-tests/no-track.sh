bad482523743dfbd92cfd11f56048d5222015be4
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-20 00:08:41 +1100

    Update no-track.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/no-track.sh
+++ e2e-tests/no-track.sh
@@ -2,12 +2,6 @@
 
 set -euxo pipefail
 
-unset CARGO_INSTALL_ROOT
-
-CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
-export CARGO_HOME
-export PATH="$CARGO_HOME/bin:$PATH"
-
 "$1" binstall -y cargo-binstall@0.20.1
 cargo-binstall --help >/dev/null
 
@@ -22,7 +16,6 @@ if [ "$exit_code" != 88 ]; then
     echo "Expected exit code 88 BinFile Error, but actual exit code $exit_code"
     exit 1
 fi
-
 
 "$1" binstall -y --no-track --force cargo-binstall@0.20.1
 cargo-binstall --help >/dev/null
