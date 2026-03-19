ac49f8de24662d81741a3ef0c62df3faac2f0233
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-19 21:28:57 +1100

    Update continue-on-failure.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/continue-on-failure.sh
+++ e2e-tests/continue-on-failure.sh
@@ -2,18 +2,6 @@
 
 set -euxo pipefail
 
-unset CARGO_INSTALL_ROOT
-
-CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
-export CARGO_HOME
-othertmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-test')
-export PATH="$CARGO_HOME/bin:$othertmpdir/bin:$PATH"
-
-mkdir -p "$othertmpdir/bin"
-# Copy it to bin to test use of env var `CARGO`
-cp "$1" "$othertmpdir/bin/cargo-binstall"
-
-
 ## Test --continue-on-failure
 set +e
 cargo binstall --no-confirm --continue-on-failure cargo-watch@8.4.0 non-existent-clippy
@@ -25,7 +13,6 @@ if [ "$exit_code" != 76 ]; then
     echo "Expected exit code 76, but actual exit code $exit_code"
     exit 1
 fi
-
 
 cargo_watch_version="$(cargo watch -V)"
 echo "$cargo_watch_version"
