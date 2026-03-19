0d438b3c44d427339c67dee2372db42968423a5e
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-20 00:09:10 +1100

    Update other-repos.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/other-repos.sh
+++ e2e-tests/other-repos.sh
@@ -2,12 +2,6 @@
 
 set -euxo pipefail
 
-unset CARGO_INSTALL_ROOT
-
-CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
-export CARGO_HOME
-export PATH="$CARGO_HOME/bin:$PATH"
-
 # Test default GitLab pkg-url templates
 #"$1" binstall \
 #    --force \
