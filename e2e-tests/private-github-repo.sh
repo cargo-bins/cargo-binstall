2b404f23feeda9122837cbc25e4963f312d7355d
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-03-20 00:11:13 +1100

    Update private-github-repo.sh
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- e2e-tests/private-github-repo.sh
+++ e2e-tests/private-github-repo.sh
@@ -2,12 +2,6 @@
 
 set -euxo pipefail
 
-unset CARGO_INSTALL_ROOT
-
-CARGO_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t 'cargo-home')
-export CARGO_HOME
-export PATH="$CARGO_HOME/bin:$PATH"
-
 # Install binaries using `--manifest-path`
 # Also test default github template
 "$1" binstall --force --manifest-path "manifests/private-github-repo-test-Cargo.toml" --no-confirm cargo-binstall --strategies crate-meta-data
