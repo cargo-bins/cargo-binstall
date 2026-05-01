7da30cefdd8e5b98fa5eb2e9f8fdc3eb8dbfe7d4
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-05-02 00:11:14 +1000

    Enable dnssec validation in resolver options
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- crates/binstalk-downloader/src/remote/resolver.rs
+++ crates/binstalk-downloader/src/remote/resolver.rs
@@ -103,7 +103,10 @@ fn new_resolver() -> Result<TokioAsyncResolver, BoxError> {
     let (config, mut opts) = get_configs()?;
 
     debug!("Resolver configuration complete");
+
+    opts.validate = true;
     opts.ip_strategy = LookupIpStrategy::Ipv4AndIpv6;
+
     let mut builder = TokioAsyncResolver::builder_with_config(config, Default::default());
     *builder.options_mut() = opts;
     Ok(builder.build()?)
